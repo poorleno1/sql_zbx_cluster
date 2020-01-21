ac35c6314a7594a1d5bbbcab0f9c7a1d4450bd9a
Poor Leno <jarekole@gmail.com>
2020-01-21 16:12:06 +0100

    encodig fix

--- scripts/mssql_version.ps1
+++ scripts/mssql_version.ps1
@@ -1,5 +1,4 @@
-﻿#
-$SQLServer = get-clusterresource -ErrorAction SilentlyContinue | where-object { $_.ResourceType -like “SQL Server” } | get-clusterparameter VirtualServerName | select-object -ExpandProperty value
+﻿$SQLServer = get-clusterresource -ErrorAction SilentlyContinue | where-object { $_.ResourceType -like “SQL Server” } | get-clusterparameter VirtualServerName | select-object -ExpandProperty value
 $ver = Invoke-Sqlcmd -Query "SELECT @@VERSION;" -QueryTimeout 3 -ServerInstance $SQLServer
 
 write-host $ver.Column1
